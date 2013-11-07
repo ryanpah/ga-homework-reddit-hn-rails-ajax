b8e25a5f777f4391840996beb77b65c8ce7cabba
Ryan Page <rpage326@gmail.com>
2013-11-07 16:10:39 -0500

    created app. added devise gem. created db.

---
+++ hacker_news/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '40697b7cb1664fe6e230ce3a4bc2d8b217e0a66a6ad86749d5f670097d3478606ecc705fadba75fdce032fc403474a8cce28fb1b94cb746991ca85bd8f14d86a'
